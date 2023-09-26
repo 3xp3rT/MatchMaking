8fdbc313f8d130bb8824bd62ec5c674966690b67
Nakibul Shaki <nakibulshaki@hotmail.com>
2023-09-25 21:23:26 -0400

    add nav component add client service

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from 'src/_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+  loggedIn:boolean = false;
+  constructor(private _accountService :AccountService) { }
+  ngOnInit(): void {
+
+  }
+  login() {
+    this._accountService.login(this.model)
+    .subscribe(
+      {
+        next:response=> {
+          console.log(response);
+          this.loggedIn= true;
+        },
+        error:er=>console.log(er),
+        complete:()=> console.log('login request has completed')
+
+      })
+  }
+}
